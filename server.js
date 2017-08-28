a5d21e558bcd85b4b0d4101fc788004252c4179f
System <none>
2017-08-28 14:06:40 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -43,16 +43,12 @@ var MongoClient = mongodb.MongoClient;
 
 // Connection URL. This is where your mongodb server is running.
 
-//(Focus on This Variable)
-var url = 'mongodb://localhost:27017/my_database_name';      
-//(Focus on This Variable)
-
 // Use connect method to connect to the Server
-  MongoClient.connect(url, function (err, db) {
+  MongoClient.connect(mongoDbUrl, function (err, db) {
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
-    console.log('Connection established to', url);
+    console.log('Connection established to', mongoDbUrl);
 
     // do some work here with the database.
 
